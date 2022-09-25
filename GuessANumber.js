93df7d376cda1d6ee6b26cf0d06af74381ad7270
SongArm <113346555+SongArm@users.noreply.github.com>
2022-09-25 20:55:11 +0300

    Project

---
+++ GuessANumber.js
@@ -0,0 +1,37 @@
+function guess() {
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    let computerGuess = Math.floor(Math.random() * 100)
+    let guess
+
+    let recursiveAsyncReadline = function () {
+        readline.question('Guess the number (0-100): ', number => {
+            guess = Number(number)
+
+
+            recursiveAsyncReadline();
+
+            if (guess <= 100 && guess >= 0) {
+                if (guess == computerGuess) {
+                    console.log('You guessed it!');
+                    return readline.close()
+                }
+             else if (guess < computerGuess) {
+                console.log('Too Low!');
+                recursiveAsyncReadline()
+            } else if (guess > computerGuess) {
+                console.log('Too High!');
+                recursiveAsyncReadline()
+            }} else {
+                console.log('Invalid input! Try again...');
+                recursiveAsyncReadline()
+            }
+        })
+    }
+    recursiveAsyncReadline()
+}
+guess()
